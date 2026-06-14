b529a392dac020053d897de05319190023606c4e
Mishita Soni <mish.soni.verma@gmail.com>
2026-06-14 14:14:45 +0530

    some changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseURL:"https://task-allotment-webapp.onrender.com",
 });
 
 export default API;
